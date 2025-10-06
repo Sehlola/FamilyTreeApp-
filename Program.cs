42e4f42e3f9acb7ae1bddce91e8a9a9b39cb37d2
Thabo Sehlola <150679153+Sehlola@users.noreply.github.com>
2025-10-06 20:31:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,23 +3,34 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add support for Blazor pages
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Connect to a local SQLite database file
 builder.Services.AddDbContext<FamilyDbContext>(options =>
     options.UseSqlite("Data Source=familytree.db"));
+
+// Add FamilyService so we can use it to get family data
 builder.Services.AddScoped<FamilyService>();
 
 var app = builder.Build();
 
+// Show an error page if the app is not in development
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
 
+// Allow use of CSS, JS, and image files
 app.UseStaticFiles();
+
+// Enable page navigation in Blazor
 app.UseRouting();
+
+// Tell the app to use Blazor and a default page
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+// Run the app
 app.Run();
